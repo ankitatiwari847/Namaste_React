6d52992a5faa0c76e7e60f8c86b78d6360fc1625
ankitatiwari847 <ankitatiwari847@gmail.com>
2023-10-26 13:07:17 +0530

    React-episode-1

---
+++ App.js
@@ -0,0 +1,7 @@
+let heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from React!"
+);
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
